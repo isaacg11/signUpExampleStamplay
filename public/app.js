d7167ed4999bb421fa8f60f1a391364f9d82b95c
isaacg11 <isaacgreywebdeveloper@gmail.com>
2015-10-16 16:20:43 -0700

    sign up by email & pass works

---
+++ public/app.js
@@ -0,0 +1,18 @@
+Stamplay.init("signuptutorial");
+
+function signUp() {
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+
+	var registrationData = {
+  		email : email,
+  		password: password
+	};
+	var newUser = new Stamplay.User().Model;
+	newUser.signup(registrationData).then(function(){
+	document.getElementById("email").value = "";
+	document.getElementById("password").value = "";
+
+	Materialize.toast('Success!', 4000);
+});
+}
